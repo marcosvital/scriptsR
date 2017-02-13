9876ff2e13a2d5d20905c051203eca9e6f3bc350
Marcos Vital (LEQ-UFAL) <marcosvital@gmail.com>
2017-02-13 10:44:05 -0200

    Add files via upload

---
+++ BioestatisticaUFAL/Script 2 - 2016-2.R
@@ -0,0 +1,44 @@
+######################################
+#Script 2 de bioestatística###########
+#Média, mediana, histogramas e etcs###
+#Prof Marcos Vital UFAL###############
+#13-02-2017###########################
+######################################
+
+##Lendo o arquivo:
+
+dados<-read.table(file.choose(), header=T) #Abra o arquivo prática1.txt
+summary(dados)								  #Aqui conferimos os dados
+attach(dados)								  #Aqui fazemos o R reconhecer as colunas 
+
+############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+0, 0, 1, 2, 63, 61, 27, 13
